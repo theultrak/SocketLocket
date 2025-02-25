d759b48cd04fb9ee992c1a05e870d37141010a0b
Brennen McCorison <brennenmc5@gmail.com>
2025-02-25 14:45:15 -0500

    Create icmpClient.py

---
+++ icmpClient.py
@@ -0,0 +1,19 @@
+from socket import *
+
+def openSocket():
+    try:
+        icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+        print("Socket Open")
+    except PermissionError as e:
+        print(f"Error: {e}. Run with admin access.")
+        exit()
+
+def packetConstruction():
+    print("Constructing packet")
+
+def pingSend():
+    print("Pinging processes beginning")
+
+openSocket()
+packetConstruction()
+pingSend()
